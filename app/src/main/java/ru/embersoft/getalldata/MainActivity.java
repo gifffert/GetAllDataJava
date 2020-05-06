345b31c8e96bcd76ca0721ca5489ba54fd3709af
Nick Titov <titonick174@icloud.com>
2020-05-06 16:07:33 +0900

    Initial commit

---
+++ app/src/main/java/ru/embersoft/getalldata/MainActivity.java
@@ -0,0 +1,58 @@
+package ru.embersoft.getalldata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        final ArrayList<String> imageList = new ArrayList<>();
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final ImageAdapter adapter = new ImageAdapter(imageList, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Images");
+
+        progressBar.setVisibility(View.VISIBLE);
+        storageReference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference fileRef : listResult.getItems()) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            imageList.add(uri.toString());
+                            Log.d("item", uri.toString());
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            recyclerView.setAdapter(adapter);
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+}
